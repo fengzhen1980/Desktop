1ff66bf0300030a8c85c205bc8cfca9fbac8ccc8
fengzhen1980 <tj.fengzhen@hotmail.com>
2023-01-15 15:04:07 -0500

    new

---
+++ DistanceTraveledWin.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Interaction logic for DistanceTraveledWin.xaml
+    /// </summary>
+    public partial class DistanceTraveledWin : Window
+    {
+        public DistanceTraveledWin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double speed = Convert.ToDouble(speedTextBox.Text);
+            double hours = Convert.ToDouble(hoursTextBox.Text);
+            double distance = 0;
+            string resultString = "Hour\tDistance Traveled\n";
+
+            for (int i = 1; i <= hours; i++)
+            {
+                distance = distance + speed;
+                resultString = resultString + i.ToString() + "\t" + distance + "\n";
+                
+            }
+            resultTextBox.Text = resultString;
+            resultTextBox.IsReadOnly = true;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] operations { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            operations = new string[] { "Test Scores", "Bank Charges", "Shipping Charges", "Distance Traveled" };
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)comboBox.SelectedItem == "Test Scores")
+            {
+                TestScoresWin testScoresWin = new TestScoresWin(); 
+                this.Close();
+                testScoresWin.Show();
+            }
+            else if ((string)comboBox.SelectedItem == "Bank Charges")
+            {
+                BankChargeWin bankChargeWin = new BankChargeWin();
+                this.Close();
+                bankChargeWin.Show();
+            }
+            else if ((string)comboBox.SelectedItem == "Shipping Charges")
+            {
+                ShippingChargesWin shippingChargesWin = new ShippingChargesWin();
+                this.Close();
+                shippingChargesWin.Show();
+            }
+            else if ((string)comboBox.SelectedItem == "Distance Traveled")
+            {
+                DistanceTraveledWin distanceTraveledWin = new DistanceTraveledWin();
+                this.Close();
+                distanceTraveledWin.Show();
+            }
+        }
+    }
+}
